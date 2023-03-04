71266c16e38be095d729bc4834921831b409b6a9
Mayya <majko2594@gmail.com>
2023-03-04 12:31:07 +0300

    add Цикл в цикле

---
+++ Lection_3_04_03_2023/Цикл_в_цикле/Program.cs
@@ -0,0 +1,19 @@
+﻿// Конструкция Цикл в цикле
+// Вывод таблицы умножения
+
+// for (int i = 2; i <= 10; i++)
+// {
+//     for (int j = 2; j <= 10; j++)
+//     {
+//         Console.WriteLine($"{i} x {j} = {i * j}");
+//     }
+// }
+
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 2; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i * j}");
+    }
+    Console.WriteLine();
+}
