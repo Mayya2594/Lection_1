680662d91b3e7a79fcdd4912b8ff35f4f5cb3ee2
Mayya <majko2594@gmail.com>
2023-03-19 22:04:39 +0300

    add Lection 7 Example_7_1, 7_2, 7_3

---
+++ Lection_7_19_03_2023/Example_7_1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 1. Собрать строку с числами от a до b, a <= b.
+
+string NumbersFor (int a, int b)
+{
+    string result = string.Empty;
+    for (int i = a; i <= b; i++)
+        result += $"{i} ";
+    return result;
+}
+
+string NumbersRec (int a, int b)
+{
+    if (a <= b) return NumbersRec (a + 1, b) + $"{a} "; // ???
+    else return string.Empty;
+}
+
+Console.WriteLine(NumbersFor(1, 10));
+Console.WriteLine(NumbersRec(1, 10));
---
+++ Lection_7_19_03_2023/Example_7_3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 3. Найти факториал числа.
+
+int FactorialFor (int n)
+{
+    int result = 1;
+    for(int i = 1; i <= n; i++) result *= i;
+    return result;
+}
+
+int FactorialRec (int n)
+{
+    if (n == 0 || n == 1) return 1;
+    else return n * FactorialRec(n - 1);
+}
+
+System.Console.WriteLine(FactorialFor(5));
+System.Console.WriteLine(FactorialRec(5));
