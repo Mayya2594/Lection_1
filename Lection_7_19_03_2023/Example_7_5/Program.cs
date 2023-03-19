e2cd53122c12d8fc35d5c2158fd27e13879e70a0
Mayya <majko2594@gmail.com>
2023-03-19 22:51:12 +0300

    add Lection 7 Example_7_4, 7_5, 7_6

---
+++ Lection_7_19_03_2023/Example_7_5/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 5. В некотором машинном алфавите имеются четыре буквы:
+//  "а", "и", "с" и "в".
+// Покажите все слова, состоящие из Т букв,
+// которые можно построить из бкув этого алфавита.
+
+char[] s = {'а', 'и', 'с', 'в'};
+int count = s.Length;
+int n = 1;
+for (int i = 0; i < count; i++)
+{
+    for(int j = 0; j < count; j++)
+    {
+        System.Console.WriteLine($"{n++, -5} {s[i]} {s[j]}");
+    }
+}
+
+System.Console.WriteLine();
+
+int t = 1;
+
+void FindWords(string alphabet, char[] word, int length = 0)
+{
+    if(length == word.Length)
+    {
+        System.Console.WriteLine($"{t++} {new String(word)}"); return;
+    }
+    for (int i = 0; i < alphabet.Length; i++)
+    {
+        word[length] = alphabet[i];
+        FindWords(alphabet, word, length + 1);
+    }
+}
+
+FindWords("аисв", new char[2]);
---
+++ Lection_7_19_03_2023/Example_7_6/Program.cs
@@ -0,0 +1,8 @@
+﻿string path = "D:/Учеба/Знакомство с языками программирования/Lection_1/Example002_HelloUser";
+DirectoryInfo di = new DirectoryInfo(path);
+System.Console.WriteLine(di.CreationTime);
+FileInfo[] fi = di.GetFiles();
+for(int i = 0; i < fi.Length; i++)
+{
+    System.Console.WriteLine(fi[i].Name);
+}
